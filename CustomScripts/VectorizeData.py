b08387bc9d7c3162e6dde58e0d53c40ec5fbb0ce
Tom-project <56174977+Tom-project@users.noreply.github.com>
2022-01-30 21:23:17 +0000

    Update VectorizeData.py

--- CustomScripts/VectorizeData.py
+++ CustomScripts/VectorizeData.py
@@ -1,5 +1,6 @@
 from re import M
 import pefile
+import peutils
 import os
 import struct
 import logging
@@ -13,7 +14,8 @@ import datetime
 #i = 0
 API_LIST = []
 File = "C:\Windows\System32\WindowsPowerShell\\v1.0\powershell.exe"
-target = pefile.PE(File)
+pe = pefile.PE(File)
+
 
 # ------------ Initialize Log file ------------
 logging.basicConfig(filename='test1.log', encoding='utf-8', level=logging.DEBUG)
@@ -34,6 +36,14 @@ logger.addHandler(ch)
 
 MalIdentifier = int(input("Is this run using a malicious program? 1 = Yes, 0 = No "))
 
+def hashFinder():
+    pe2 = pefile.PE("C:\Windows\System32\\amsi.dll")
+    byt3s = []
+    byt3s = pe2.sections[0].get_data() #read .text section append to byt3s 
+    hash = hashlib.sha256()
+    hash.update(byt3s)
+    fHash = hash.hexdigest()
+    return fHash
 
 def extractResults(df, df2): 
     #df["Data"] = df[0]
@@ -44,10 +54,10 @@ def extractResults(df, df2):
 
     #header = ['Event', 'Action', 'Priority', 'Malicious', 'EntryPoint', 'VirtualMemSize', 'RawDataSize'] # Only needed when in write mode to create a new file
     data =[
-            [df[0],df[1],df[2],df[3]],
-            [df2[0],df2[1],df2[2],df2[3],df2[4],df2[5],df2[6]]
+            [df[0], df[1], df[2], df[3], df[4], df[5], df[6], df[7]],
+            [df2[0], df2[1], df2[2], df2[3], df2[4], df2[5], df2[6], df2[7]]
           ]
-    with open(r"C:\\Users\\thoma\Documents\dataset.csv", "a") as f: 
+    with open(r"C:\\Users\\thoma\Documents\dataset2.csv", "a") as f: 
         # create the csv writer
         writer = csv.writer(f)
 
@@ -61,8 +71,10 @@ def extractResults(df, df2):
 def APIPull():
     print("API Pull is running at time: " + str(int(time.time())) + " seconds.")
     i = 0
+    fHash = hashFinder()
+
     # Pulls APIs from PE
-    for entry in target.DIRECTORY_ENTRY_IMPORT:
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
         for API in entry.imports:
             API_LIST.append(API.name)
 
@@ -77,18 +89,22 @@ def APIPull():
         API_LIST[i] = API_LIST[i].decode('utf-8')
         i = i + 1 
 
+    EntryPoint = pe.sections[0].VirtualAddress
+    VirtualMemSize = pe.sections[0].Misc_VirtualSize
+    RawDataSize = pe.sections[0].SizeOfRawData
     i = 0
     while(i < 20):
         
         if ("GetProcAddress" in API_LIST): # Looking for Win API call of interest
-            #print("GetProcAddress Found")
             x = API_LIST.index("GetProcAddress")
             logger.info(API_LIST[x])
-            #b = input("Would you like to see the rest of the list? ")
-            #if b == "y":
+            """
+            b = input("Would you like to see the rest of the list? ")
+            if b == "y":
                 #print(API_LIST)
+            """
             global df 
-            df = ["GetProcAddress Found!","1","medium",MalIdentifier]
+            df = ["GetProcAddress Found!","1","medium",MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize, fHash]
             break
             
 
@@ -96,61 +112,46 @@ def APIPull():
             i = i + 1
             if (i == 20):
                 print("NOT FOUND", API_LIST)
-                df = ["GetProcAddress NOT FOUND","0","low",MalIdentifier]
+                df = ["GetProcAddress NOT FOUND","0","low",MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize, fHash]
         time.sleep(5)
             
 
 
 def amsiCheck():
     print("AMSI Check is running at time: " + str(int(time.time())) + " seconds.")
-     
+    
     pe2 = pefile.PE("C:\Windows\System32\\amsi.dll")
-
     for section in pe2.sections:
         print(section.Name, "Virtual Address: ", hex(section.VirtualAddress), "Virtual Size: ",  hex(section.Misc_VirtualSize),
                     "Raw data size: ", section.SizeOfRawData )
-
-    byt3s = []
-    byt3s = pe2.sections[0].get_data() #read .text section append to byt3s 
-    hash = hashlib.sha256()
-    hash.update(byt3s)
-    fHash = hash.hexdigest()
-    #print("hash is ------------:",pe2.sections[0].Name, "{0}".format(fHash))
+    
     EntryPoint = pe2.sections[0].VirtualAddress
     VirtualMemSize = pe2.sections[0].Misc_VirtualSize
     RawDataSize = pe2.sections[0].SizeOfRawData
 
-    print("Entry point is: ", EntryPoint)
-    print("v mem is: ", VirtualMemSize)
-    print("raw data is: ", RawDataSize)
-    
+    fHash = hashFinder()
+
     i = 0
-    d=0
     while(i<21):
-        hash = hashlib.sha256()
-        byt3s = pe2.sections[0].get_data() #read .text section append to byt3s 
-        hash.update(byt3s)
-        fHash2 = hash.hexdigest()
-        
+        fHash2 = hashFinder()
         if fHash!=fHash2:
             x = logger.alert("Memory Patching Detected! ")
-            df2 = [x,"1","high", MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize]
+            df2 = [x,"1","high", MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize, fHash]
             extractResults(df, df2)
             break
         
         else:
             print("Continuing. ")
             #print("hash of .text is: {0}".format(fHash))
-            #byt3s = pe2.sections[d].get_data() #read all sections append to byt3s 
-            #hash.update(byt3s)
-            #fHash3 = hash.hexdigest()
+            #fHash3 = hashFinder()
             #print("hash is:",pe2.sections[d].Name, "{0}".format(fHash3))
             #d=d+1 #eventually reaches end of pe2.sections array - needs a fix
             i = i + 1
-
-            if (i > 2):
-                df2 = ["Memory Patching NOT Detected","0","low", MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize] 
+            
+            if (i > 2): #if (i == 21):
+                df2 = ["Memory Patching NOT Detected","0","low", MalIdentifier, EntryPoint, VirtualMemSize, RawDataSize, fHash] 
                 extractResults(df, df2)
+                #break
         time.sleep(5)
     # ---------------------------
 
